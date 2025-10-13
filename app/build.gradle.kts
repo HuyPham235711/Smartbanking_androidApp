c4cfd26243aab38836036088e57ec95202d36cc9
HuyPham235711 <g4m3sf0rhuy@gmail.com>
2025-10-13 17:21:27 +0700

    20251013_đồng bộ cấu hình Android Studio và Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,37 +9,32 @@ plugins {
 android {
     namespace = "com.example.afinal"
     compileSdk = 36
-
     defaultConfig {
         applicationId = "com.example.afinal"
-        minSdk = 34
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
+        viewBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.7.3"
     }
 }
+
+
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -55,7 +50,11 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
 
     // Analytics (tuỳ chọn)
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("com.google.firebase:firebase-analytics")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
